fb97d4e622eb7c1edb61092cabaa9cf3d5553766
Oliwier Kaspprzak <oliwier.kasprzak00gmail.com>
2023-04-25 21:23:26 +0200

    Initial Commit

---
+++ FeedbackHub/FeedbackHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  FeedbackHubApp.swift
+//  FeedbackHub
+//
+//  Created by Oliwier Kasprzak on 25/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct FeedbackHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
