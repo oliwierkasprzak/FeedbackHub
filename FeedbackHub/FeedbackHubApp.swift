f8473df90be1d024a5f42318bbda0898befed08e
Oliwier Kaspprzak <oliwier.kasprzak00gmail.com>
2023-05-09 20:23:11 +0200

    Synchronising the saving of the app.

--- FeedbackHub/FeedbackHubApp.swift
+++ FeedbackHub/FeedbackHubApp.swift
@@ -10,6 +10,7 @@ import SwiftUI
 @main
 struct FeedbackHubApp: App {
     @StateObject var dataController = DataController()
+    @Environment(\.scenePhase) var scenePhase
     
     var body: some Scene {
         WindowGroup {
@@ -22,6 +23,11 @@ struct FeedbackHubApp: App {
             }
             .environment(\.managedObjectContext, dataController.container.viewContext)
             .environmentObject(dataController)
+            .onChange(of: scenePhase) { phase in
+                if phase != .active {
+                    dataController.save()
+                }
+            }
         }
     }
 }
