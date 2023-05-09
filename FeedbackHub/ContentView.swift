1ac2b6c390babae8d1d134c01bcf99820e276eea
Oliwier Kaspprzak <oliwier.kasprzak00gmail.com>
2023-05-09 12:00:16 +0200

    Showing and deleting issues is working correctly.

--- FeedbackHub/ContentView.swift
+++ FeedbackHub/ContentView.swift
@@ -13,28 +13,35 @@ struct ContentView: View {
     var issues: [Issue] {
         let filter = dataController.selectedFilter ?? .all
         var allIssues: [Issue]
-
+        
         if let tag = filter.tag {
             allIssues = tag.issues?.allObjects as? [Issue] ?? []
         } else {
             let request = Issue.fetchRequest()
+            request.predicate = NSPredicate(format: "modificationDate > %@", filter.minModificationDate as NSDate)
             allIssues = (try? dataController.container.viewContext.fetch(request)) ?? []
         }
-
+        
         return allIssues.sorted()
     }
     
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        List {
+            ForEach(issues) { issue in
+                IssueRow(issue: issue)
+            }
+            .onDelete(perform: delete)
         }
-        .padding()
+        .navigationTitle("Issues")
     }
-}
-
+    
+    func delete(_ offsets: IndexSet) {
+        for offset in offsets {
+            let item = issues[offset]
+            dataController.deleteObject(item)
+        }
+    }
+} 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
