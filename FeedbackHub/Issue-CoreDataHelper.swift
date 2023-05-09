6d1010d6b421fa646acd086ce9c63f6a6920076d
Oliwier Kaspprzak <oliwier.kasprzak00gmail.com>
2023-05-09 13:02:35 +0200

    DetailView screen is finished and the editing data(tags adding, tags priority changing, adding description) and some smaller features concerning bug fixing and optimalization.

--- FeedbackHub/Issue-CoreDataHelper.swift
+++ FeedbackHub/Issue-CoreDataHelper.swift
@@ -26,6 +26,24 @@ extension Issue {
         modificationDate ?? .now
     }
     
+    var issueStatus: String {
+        if completed {
+            return "Closed"
+        } else {
+            return "Open"
+        }
+    }
+    
+    var issueTagsList: String {
+        guard let tags else { return "No tags" }
+        
+        if tags.count == 0 {
+            return "No tags"
+        } else {
+            return issueTags.map(\.tagName).formatted()
+        }
+    }
+    
     var issueTags: [Tag] {
         let result = tags?.allObjects as? [Tag] ?? []
         return result.sorted()
